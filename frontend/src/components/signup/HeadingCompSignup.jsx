7902e9b91d1ec92e302a74f947bc44af494dad8a
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-09-03 13:48:03 +0000

    Update HeadingCompSignup.jsx

--- frontend/src/components/signup/HeadingCompSignup.jsx
+++ frontend/src/components/signup/HeadingCompSignup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeadingCompSignin = ({first,second}) => {
   return (
     <div>
-        <h1 className='text-center sign-in-heading col-lg-8 column d-flex justify-content-center align-items-center col-right' >
+        <h1 className='text-center sign-in-heading col-lg-8 column d-flex justify-content-center align-items-center' >
                             {first} <br /> {second}
                         </h1>
     </div>
